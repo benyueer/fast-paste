bc3d34e7a83ab07f517a309355d4f9d3643f8447
benyueer <347201906@qq.com>
2025-05-26 17:48:59 +0800

    feat: shortcut

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,9 @@
-import { FormItem, Input } from 'cui-solid'
-import { createSignal, For } from 'solid-js'
-import icon from '../public/icon.png'
+import RouterView from './router'
 
 function App() {
-  const [state, setState] = createSignal({
-    sk: '',
-    cList: [],
-  })
-
   return (
-    <main w-screen h-screen bg-white p-20>
-      <div w-full flex gap-20 items-center>
-        <img src={icon} w-50 h-50 />
-        <p text-20 m-0>Fast Paest</p>
-      </div>
-      <div mt-20>
-        <FormItem label="sk:">
-          <Input
-            type="password"
-            value={state().sk}
-            onInput={sk => setState({ ...state(), sk })}
-            w="[300px_!important]"
-          />
-        </FormItem>
-        <div>
-          <For each={state().cList}>
-            {
-              (item, i) => (
-                <div>i</div>
-              )
-            }
-          </For>
-        </div>
-      </div>
+    <main w-screen h-screen bg-white>
+      <RouterView />
     </main>
   )
 }
